9e932955ecbc33c87ecabe4de1de41f8a4a0fef6
NicoLopezBjj <lopeznnr@gmail.com>
2023-10-05 14:51:13 -0300

    add routes get signin and signup

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const app=express()
 app.use(express.static(path.join(__dirname,'public')))
 app.set('view engine','ejs')
 
+app.get('/signin',(req,res)=>{
+    res.render('signin')
+})
+
+app.get('/signup',(req,res)=>{
+    res.render('signup')
+})
+
 app.listen(3900,()=>{
     console.log('servidor ejectuandose')
 })
