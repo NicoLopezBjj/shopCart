edad42d4250bcfdc3574faf89efc413a8efdad85
Joakispiller <joakispiller22@gmail.com>
2023-10-06 15:29:00 -0300

    añadi mi card y margenes joaki

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express=require('express')
-const path=require('path')
-
+const authRouter=require('./routes/authRoutes')
+const path=require ('path')
 
 const app=express()
 
 app.use(express.static(path.join(__dirname,'public')))
 app.set('view engine','ejs')
 
-app.get('/',(req,res)=>{
-    res.render('home')
-})
-
-app.use(authRouter)
+app.get('/',(req,res)=>[
+    res.render('home')])
+    
+    app.use(authRouter)
 
 app.listen(3900,()=>{
     console.log('servidor ejectuandose')
