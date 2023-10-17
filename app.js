d3dcefd3c369f3ffa60b83e3ada991736511b501
NicoLopezBjj <lopeznnr@gmail.com>
2023-10-17 13:07:35 -0300

    modified route home

--- app.js
+++ app.js
@@ -12,12 +12,14 @@ const app=express()
 app.use(express.static(path.join(__dirname,'public')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
 const secretSession = process.env.secretSession
 app.use(session({
     secret: secretSession,
     resave: false,
     saveUninitialized: true
 }))
+
 app.set('view engine','ejs')
 
 // conexion a la base de datos
@@ -39,8 +41,8 @@ const connectDataBase = async () => {
 connectDataBase()
 
 // ruta principal
-app.get('/',(req,res)=>[
-    res.render('home')])
+app.get('/',(req,res)=>{
+    res.render('home')})
     
 app.use(authRouter)
 
