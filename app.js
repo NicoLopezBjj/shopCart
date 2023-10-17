cac90ff1f9b9a52d315365e57cdb97593c92b734
NicoLopezBjj <lopeznnr@gmail.com>
2023-10-17 12:54:49 -0300

    add express-session and modified bolsos.json

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express=require('express')
+const session=require('express-session')
 const authRouter=require('./routes/authRoutes')
 const path=require ('path')
 const mongoose=require('mongoose')
@@ -11,6 +12,12 @@ const app=express()
 app.use(express.static(path.join(__dirname,'public')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+const secretSession = process.env.secretSession
+app.use(session({
+    secret: secretSession,
+    resave: false,
+    saveUninitialized: true
+}))
 app.set('view engine','ejs')
 
 // conexion a la base de datos
