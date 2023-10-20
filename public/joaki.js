45ba4e0b2b7e659bff03ea565440d8971a0c55dd
Joakispiller <joakispiller22@gmail.com>
2023-10-20 19:01:50 -0300

    hice el slide

--- public/joaki.js
+++ public/joaki.js
@@ -18,3 +18,34 @@ document.addEventListener("DOMContentLoaded", function() {
         cardsContainer.appendChild(clonedCard);
     }
 });
+
+/* SLIDES DEL SHOP */
+let slider = document.querySelector(".slider-contenedor")
+let sliderIndividual = document.querySelectorAll(".contenido-slider")
+let contador = 1;
+let width = sliderIndividual[0].clientWidth;
+let intervalo = 5000; /* tiempo de pasarlo */
+
+window.addEventListener("resize", function(){
+    width = sliderIndividual[0].clientWidth;
+}) /* para q se adapte a los tamaños */
+
+setInterval(function(){
+    slides();
+},intervalo);
+
+
+
+function slides(){
+    slider.style.transform = "translate("+(-width*contador)+"px)";
+    slider.style.transition = "transform .8s";
+    contador++;
+
+    if(contador == sliderIndividual.length){
+        setTimeout(function(){
+            slider.style.transform = "translate(0px)";
+            slider.style.transition = "transform 0s";
+            contador=1;
+        },1500)
+    }
+}
