46f5923a5d00134b252cfeef7f501f8135617526
NicoLopezBjj <lopeznnr@gmail.com>
2023-10-31 11:16:12 -0300

    clean file routes/products.js

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router=express.Router()
 const products=require('../controllers/products')
-const Producto = require('../models/Products');
+
 
 
 router.get('/shop',products.shop_get)
