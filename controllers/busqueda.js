4c77ae061a969a2095a7c602f2c35ee11a73afdb
NicoLopezBjj <lopeznnr@gmail.com>
2023-11-07 13:46:58 -0300

    add no_resultados.ejs for driving search fail

--- controllers/busqueda.js
+++ controllers/busqueda.js
@@ -3,14 +3,18 @@ const Producto= require ('../models/Products')
 
 const buscar = async (req,res) =>{
     const busqueda = req.query.busqueda; // para obtener texto que el usuario escribió en el formulario
-    console.log(busqueda)
     try {
       const productosEncontrados = await Producto.find({ nombre: { $regex: new RegExp(busqueda, 'i') } });
-      res.render('resultado', { productos: productosEncontrados ,Producto,user : req.user});
+      if (productosEncontrados.length > 0) {
+        res.render('resultado', { productos: productosEncontrados ,Producto,user : req.user});
+    } else {
+        // No se encontraron productos, renderiza una página de "no se encontraron resultados"
+        res.render('no_resultados', { busqueda });
+    }
     } catch (error) {
       console.log(error);
       res.render('error404')
-    }
+    } 
 }
 
 module.exports={
