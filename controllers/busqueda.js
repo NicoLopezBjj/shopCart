04a47e192680fb3d25a393886c6b649bb14028e7
NicoLopezBjj <lopeznnr@gmail.com>
2023-11-14 11:48:15 -0300

    mejore la seccion de resultado de la busqueda

--- controllers/busqueda.js
+++ controllers/busqueda.js
@@ -6,9 +6,9 @@ const buscar = async (req,res) =>{
     const busqueda = req.query.busqueda; // para obtener texto que el usuario escribió en el formulario
     try {
       const productosEncontrados = await Producto.find({ nombre: { $regex: new RegExp(busqueda, 'i') } });//busca por nombre en la DB
-      const usuario = await User.findById(req.user._id); 
+      
       if (productosEncontrados.length > 0) { //si el resultado es mayor a 0, osea encontro un producto, renderiza resultado
-        res.render('resultado', { productos: productosEncontrados ,Producto,user : req.user, usuario});
+        res.render('resultado', { productos: productosEncontrados ,Producto,user : req.user});
     } else {
         // No se encontraron productos, renderiza una página de "no se encontraron resultados"
         res.render('no_resultados', { busqueda , user : req.user });
