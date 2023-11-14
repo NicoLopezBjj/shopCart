be14e02b8dd41a2022a3352813dce25080324ef0
NicoLopezBjj <lopeznnr@gmail.com>
2023-11-14 11:02:52 -0300

    add autenticar en mostrarProductosporid

--- routes/products.js
+++ routes/products.js
@@ -7,7 +7,7 @@ router.get('/shop',products.shop_get)
 router.get('/shop/categoria/:categoria?',products.mostrarCategoria)
 router.get('/shop/marca/:marca?',products.mostrarProductosPorMarca)
 router.get('/shop/color/:color?',products.mostrarProductosPorColor)
-router.get('/shop/producto/:id',products.mostrarProductoPorID)
+router.get('/shop/producto/:id',autenticar,products.mostrarProductoPorID)
 router.get('/shop/pagina/:pagina',products.shop_get);
   
   
