abab2a71732e0ab93f6be04a66c19c4389d0a244
NicoLopezBjj <lopeznnr@gmail.com>
2023-11-14 10:11:22 -0300

    add ips in card-carrusel-dos

--- routes/busqueda.js
+++ routes/busqueda.js
@@ -3,6 +3,6 @@ const router=express.Router()
 const busqueda=require('../controllers/busqueda')
 const autenticar=require('../middlewares/authMiddleware')
 
-router.get('/buscar',autenticar,busqueda.buscar)
+router.get('/buscar',busqueda.buscar)
 
 module.exports = router
--- routes/products.js
+++ routes/products.js
@@ -3,12 +3,12 @@ const router=express.Router()
 const products=require('../controllers/products')
 const autenticar = require('../middlewares/authMiddleware')
 
-router.get('/shop',autenticar,products.shop_get)
-router.get('/shop/categoria/:categoria?',autenticar, products.mostrarCategoria)
-router.get('/shop/marca/:marca?',autenticar,products.mostrarProductosPorMarca)
-router.get('/shop/color/:color?',autenticar,products.mostrarProductosPorColor)
-router.get('/shop/producto/:id',autenticar,products.mostrarProductoPorID)
-router.get('/shop/pagina/:pagina', autenticar, products.shop_get);
+router.get('/shop',products.shop_get)
+router.get('/shop/categoria/:categoria?',products.mostrarCategoria)
+router.get('/shop/marca/:marca?',products.mostrarProductosPorMarca)
+router.get('/shop/color/:color?',products.mostrarProductosPorColor)
+router.get('/shop/producto/:id',products.mostrarProductoPorID)
+router.get('/shop/pagina/:pagina',products.shop_get);
   
   
 
